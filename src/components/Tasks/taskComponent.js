4454caa433df0192e25f352d56913c9492699b57
David Trinh <dtrinh@dtrinh.local>
2017-06-19 15:31:35 -0700

    - modified to now render a list of habbits using the component <HabbitsList/>   - component will render a list of habbits.

--- src/components/Tasks/taskComponent.js
+++ src/components/Tasks/taskComponent.js
@@ -1,40 +1,33 @@
 import React, { Component } from 'react';
-import {Card, CardText, CardHeader} from 'material-ui/Card';
+
 import FloatingActionButton from 'material-ui/FloatingActionButton';
 import ContentAdd from 'material-ui/svg-icons/content/add';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+
+import HabbitsList from './HabbitsList/HabbitsList';
 import './taskComponent.css';
 //import FlatButton from 'material-ui/FlatButton';
 
+const habbits = [
+  {id: 1, habbitTitle: "URL Avatar", habbitSubTitle: "Subtitle", habbitDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elite, donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi."},
+  {id: 2, habbitTitle: "URL Avatar", habbitSubTitle: "Subtitle", habbitDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec mattis pretium massa." },
+  {id: 3, habbitTitle: "URL Avatar", habbitSubTitle: "Subtitle", habbitDescription: "Nunc lacinia auctor quam sed pellentesque. Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio."},
+  {id: 4}
+];
+
+injectTapEventPlugin();
+
 class TaskComponent extends Component {
+
   render() {
+
     return (
       <div className="taskCompontent flexcontainer">
         <div className="row">
           <FloatingActionButton className="addTask-btn-style" secondary={true}>
             <ContentAdd />
           </FloatingActionButton>
-          <Card className="taskItem">
-            <CardHeader className="taskTitle" title="URL Avatar" subtitle="Subtitle">
-            </CardHeader>
-            <CardText className="cardText">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
-            </CardText>
-          </Card>
-          <Card className="taskItem">
-            <CardHeader className="taskTitle" title="URL Avatar" subtitle="Subtitle"/>
-            <CardText className="cardText">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              Donec mattis pretium massa.
-            </CardText>
-          </Card>
-          <Card className="taskItem">
-            <CardHeader className="taskTitle" title="URL Avatar" subtitle="Subtitle"/>
-            <CardText className="cardText">
-              Nunc lacinia auctor quam sed pellentesque.
-              Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
-            </CardText>
-          </Card>
+          <HabbitsList habbits={habbits}/>
         </div>
       </div>
     );
